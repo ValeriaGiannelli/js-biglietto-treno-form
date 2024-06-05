59436a9e54c55c9e524858eeaba2c205c3700814
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-05 16:03:39 +0200

    Inserimento nel biglietto di nome + offerta

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
 
 
 let ticketPrice;
+let offer;
 // prendo l'elemento che deve cliccare la persona
 const generate = document.querySelector(".genera");
 console.log(generate);
@@ -25,6 +26,10 @@ generate.addEventListener('click',
         let ageUser = parseInt(document.getElementById("age").value);
         console.log("L'età della persona è:", ageUser);
 
+        // prendo il nome inserito dalla persona e lo metto in una variabile
+        let nameUser = document.getElementById("user_name").value;
+        console.log("Il nome della persona è:", nameUser);
+
         // calcolo prezzo totale
         let fullPrice = km * 0.21;
         console.log("il prezzo totale è:", fullPrice);
@@ -38,12 +43,15 @@ generate.addEventListener('click',
 
         if(ageUser < 18){ // se età minore 18 -> sconto 20%
                 ticketPrice = fullPrice - discount20;
+                offer = "Biglietto scontato 20%";
                 console.log("hai diritto a:", discount20, "quindi ti costa:", ticketPrice);
             } else if (ageUser > 65){ // se età maggiore 65 -> sconto 40%
                 ticketPrice = fullPrice - discount40;
+                offer = "Biglietto scontato 40%";
                 console.log("hai diritto a:", discount40, "quindi ti costa:", ticketPrice);
             } else { // altrimenti prezzo intero 0.21 * numero chilometri
                 ticketPrice = fullPrice;
+                offer = "Biglietto Standard";
                 console.log("non hai sconti. Qundi ti costa:", ticketPrice);
             }
         
@@ -53,8 +61,14 @@ generate.addEventListener('click',
         let ticketPriceRounded = ticketPrice.toFixed(2);
         console.log("il costo del tuo biglietto è di:", ticketPriceRounded);   
         
-        // output a schermo: "il prezzo del biglietto è: "
+        // inserisco il costo del biglietto nel documento
         document.querySelector(".price").innerHTML = `${ticketPriceRounded} €`;
+
+        // inserisco il nome della persona nel documento
+        document.getElementById("input_name").innerHTML = nameUser;
+
+        // inserisco il tipo di offerta che ha la persona
+        document.getElementById("offerta").innerHTML = offer;
         
         // mostrare il biglietto aggiungendo all'elemento la classe "show" che ho dato in css
         displayTicket.classList.add("show");
